780066e39c06e2708618e10325eda8db2b467f81
Jahongir <jahongirhakimjonov@gmail.com>
2024-03-31 00:40:38 +0500

    Django Default

---
+++ core/swagger.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+urlpatterns = [
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+]
---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from .swagger import urlpatterns as swagger_patterns
+from core import settings
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
+urlpatterns += swagger_patterns
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
