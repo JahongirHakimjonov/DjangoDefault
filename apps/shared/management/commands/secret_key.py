f49fb0aae48dd9348f0fecee7d7a6c1d93d3040c
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2024-06-25 17:49:44 +0500

    upgrade

--- apps/shared/management/commands/secret_key.py
+++ apps/shared/management/commands/secret_key.py
@@ -4,10 +4,18 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'Generates a new Django secret key'
+    help = "Generates a new Django secret key"
 
     def handle(self, *args, **options):
-        print(Fore.LIGHTCYAN_EX + "<======================================================>")
-        print("<=" + Style.RESET_ALL, Fore.LIGHTMAGENTA_EX + utils.get_random_secret_key() + Style.RESET_ALL,
-              Fore.LIGHTCYAN_EX + "=>")
-        print("<======================================================>" + Style.RESET_ALL)
+        print(
+            Fore.LIGHTCYAN_EX
+            + "<======================================================>"
+        )
+        print(
+            "<=" + Style.RESET_ALL,
+            Fore.LIGHTMAGENTA_EX + utils.get_random_secret_key() + Style.RESET_ALL,
+            Fore.LIGHTCYAN_EX + "=>",
+        )
+        print(
+            "<======================================================>" + Style.RESET_ALL
+        )
