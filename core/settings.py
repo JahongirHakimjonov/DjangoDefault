3ff5baf8e9474d9270c5c995d6d4f24a5829742b
JahongirHakimjomov <jahongirhakimjonov@gmail.com>
2025-01-29 18:41:35 +0500

    fix: db conf fix

--- core/settings.py
+++ core/settings.py
@@ -70,8 +70,8 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
+        "HOST": os.getenv("POSTGRES_BOUNCER_HOST"),
+        "PORT": os.getenv("POSTGRES_BOUNCER_PORT"),
     }
 }
 
