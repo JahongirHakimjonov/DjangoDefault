065d372c2e0efe352bf85cb0c62ca5b7c080631e
JahongirHakimjomov <jahongirhakimjonov@gmail.com>
2025-02-22 18:04:52 +0500

    fix: update list_display to include user ID in admin panel

--- apps/shared/admin/base.py
+++ apps/shared/admin/base.py
@@ -21,7 +21,7 @@ class UserAdmin(BaseUserAdmin, ModelAdmin):
     change_password_form = AdminPasswordChangeForm
     add_form = UserCreationForm
     form = UserChangeForm
-    list_display = ("username", "email", "is_active", "is_staff", "is_superuser")
+    list_display = ("id", "username", "email", "is_active", "is_staff", "is_superuser")
     list_filter = ("is_active", "is_staff", "is_superuser")
     search_fields = ("username", "email")
     ordering = ("username",)
