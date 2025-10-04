59704f1cd250b28ccd47b24e0650b3c84e6f1caf
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2025-10-05 00:32:09 +0500

    feat: integrate Sentry and Prometheus for error monitoring and metrics tracking

--- src/core/urls.py
+++ src/core/urls.py
@@ -13,21 +13,21 @@ from apps.shared.views.base import (
 from core.config.swagger import urlpatterns as swagger_patterns
 
 urlpatterns = (
-        [
-            path("i18n/", include("django.conf.urls.i18n")),
-        ]
-        + i18n_patterns(
-    path("admin/", admin.site.urls),
-)
-        + [
-            path("", include("apps.shared.urls")),
-            path("ckeditor5/", include("django_ckeditor_5.urls")),
-            path("rosetta/", include("rosetta.urls")),
-            path("", include("django_prometheus.urls")),
-            # Media and static files
-            re_path(r"static/(?P<path>.*)", serve, {"document_root": settings.STATIC_ROOT}),
-            re_path(r"media/(?P<path>.*)", serve, {"document_root": settings.MEDIA_ROOT}),
-        ]
+    [
+        path("i18n/", include("django.conf.urls.i18n")),
+    ]
+    + i18n_patterns(
+        path("admin/", admin.site.urls),
+    )
+    + [
+        path("", include("apps.shared.urls")),
+        path("ckeditor5/", include("django_ckeditor_5.urls")),
+        path("rosetta/", include("rosetta.urls")),
+        path("", include("django_prometheus.urls")),
+        # Media and static files
+        re_path(r"static/(?P<path>.*)", serve, {"document_root": settings.STATIC_ROOT}),
+        re_path(r"media/(?P<path>.*)", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
 )
 
 urlpatterns += swagger_patterns
